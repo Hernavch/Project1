4a8e289de9329f36e6273a5b7e6cf9ccedfd0666
Hernavch <hernandezvc1292@gmail.com>
2019-02-12 02:57:55 -0800

    connected ids to images

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,11 +28,12 @@ $(document).ready(function(){
 //Search Function On click function
 $("#searchbtn").on("click", function(){ 
     event.preventDefault();
-    
+    var movie=false;
     var type;
 // Movie Radio
     if ($("#movieradio").is(":checked")){
     type="movie"
+    movie=true;
     }
     else{
         type="person"
@@ -44,7 +45,7 @@ $("#searchbtn").on("click", function(){
 //OMDP API Ajax call
     var search = $("#user-input").val();
 
-    var queryURL = "https://api.themoviedb.org/3/search/"+ type +"?api_key=f0af9ea07b16056057fccc931b462c5f&query="+ search;
+    var queryURL = "https://api.themoviedb.org/3/search/"+ type +"?api_key=f0af9ea07b16056057fccc931b462c5f&query="+ search + "&append_to_response=credits";
     
 
     $.ajax({
@@ -63,39 +64,41 @@ $("#searchbtn").on("click", function(){
              if (type=== "person"){
 
                 var actor= information[i].name;
+                var idNumber= information[i].id;
                 console.log(actor);
                 var actorPic=information[i].profile_path;
                 var actorWorks= information[i].known_for;
                 // insert photos into carousel
                 var actorImg=$("<img>");
                   actorImg.attr("id","actor-"+ i);
+                  actorImg.attr("type", "idNumber")
+                  actorImg.attr("class", "images");
                   actorImg.attr("alt", "actor");
                   actorImg.attr("src", "http://image.tmdb.org/t/p/w185/" + actorPic);
                   $("#carousel-"+ i).empty().append(actorImg);
 
                 var actorName=$("<label>");
                   actorName.attr("for", "actor-"+i);
+                  actorName.attr("type", idNumber);
                   $("#carousel-"+ i).prepend(actor);
+                  $("#carousel-"+ i).append(idNumber);
 
                       // //For loop to show actorswork  
                       for(var j=0; j < actorWorks.length; j++){
                         var actorMovie= actorWorks[j];
                         }
                   
-
-                      
              }
               // IF search is Movie 
              else {
-               
                 var title= information[i].title;
                 var poster= information[i].poster_path;
                 var movieSum=information[i].overview;
 
-                console.log(poster);
                 // insert image in carousel
                 var posterImg= $("<img>");
                   posterImg.attr("id", "poster-"+ i);
+                  posterImg.attr("class", "images");
                   posterImg.attr("alt","movie");
                   posterImg.attr("src", "http://image.tmdb.org/t/p/w185/" + information[i].poster_path);
                 $("#carousel-"+ i).empty().append(posterImg);
@@ -103,12 +106,39 @@ $("#searchbtn").on("click", function(){
                 var posterName= $("<label>");
                   posterName.attr("for", "poster-"+i);
                   $("#carousel-"+ i).prepend(title);
-                                             
-                // for loop to change id number for image
               
-                
-            }
+                }
         }
+    
+        $(document.body).on("click", ".images", function(){
+          var chosen= $(this);
+          $("#mainImage").empty().append(chosen.clone());
+          
+          if(movie===false){
+                      
+            $.ajax({
+              url:"https://api.themoviedb.org/3/person/"+ idNumber +"?api_key=f0af9ea07b16056057fccc931b462c5f&language=en-US&adult=false",
+              method:"GET"
+      
+            }).then(function(response2){
+              console.log(response2);
+              $("#summary").empty().append(response2.biography);
+              $("#mainName").empty().append(response2.name);
+              $("#dates").empty().append(response2.birthday);
+
+
+               });
+
+
+          }else{
+            
+            $("#summary").append(movieSum);
+            console.log(movieSum);
+          };
+
+
+      
+        });
         
     
       
